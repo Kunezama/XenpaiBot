b3176fbda269b5cb93185d6db812286644fa5cb8
Kanezama <gengsuki7@gmail.com>
2025-03-14 20:30:45 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,7 @@ echo "Installing dependencies..."
 pkg update -y 2>/dev/null || sudo apt update -y
 pkg install python -y 2>/dev/null || sudo apt install python3 -y
 
+echo "Installing Python packages..."
+pip install -r requirements.txt 2>/dev/null || pip3 install -r requirements.txt
+
 echo "Installation complete!"
